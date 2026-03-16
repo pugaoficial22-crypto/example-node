4ed1dfd6c3e9e921cb5052f64d1cb3b1abbf9d33
DiegoPuga22 <20231701060@uteq.edu.mx>
2026-03-16 08:47:44 -0600

    refactor: move app logic inside fast-api-dev and fix duplication

--- fast-api-dev/src/app.js
+++ fast-api-dev/src/app.js
@@ -1,2 +1,22 @@
-// Reexport del app principal para que los tests lo consuman desde /src
-module.exports = require('../app');
+const express = require('express');
+const path = require('node:path'); // Corregido: prefijo node: y uso de const
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const itemsRouter = require('./routes/items');
+
+const app = express(); // Corregido: uso de const
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/items', itemsRouter);
+
+module.exports = app;
